f8e2306251f55b8a0b524a9053c741628a4e20d9
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-12-08 16:50:59 +0300

    Deployment to heroku

--- hospital/models.py
+++ hospital/models.py
@@ -8,7 +8,3 @@ class User(AbstractUser):
     is_admin = models.BooleanField('adminstrator status',default=False)
     is_user = models.BooleanField('regular status', default=False)
 
-class BloodDrives(models.Model):
-    hospital_name = models.CharField(max_length=200)
-    drive_location = models.TextField(max_length=400)
-    drive_date = models.DateTimeField(auto_now_add=True)
