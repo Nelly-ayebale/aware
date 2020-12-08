29a4f726a45acc011f7a6388ec09d4b68ea4487c
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-12-08 18:31:36 +0300

    Added more models

--- hospital/models.py
+++ hospital/models.py
@@ -8,3 +8,35 @@ class User(AbstractUser):
     is_admin = models.BooleanField('adminstrator status',default=False)
     is_user = models.BooleanField('regular status', default=False)
 
+class Hospital(models.Model):
+    adminstrator = models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_photo = CloudinaryField('image',blank=True,null=True)
+    hospital_name = models.CharField(max_length=400)
+    capacity_required = models.IntegerField(default=0)
+    capacity_available = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.user.username
+
+class Donor(models.Model):
+    donor = models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic = CloudinaryField('image',blank=True,null=True)
+    donor_name = models.CharField(max_length=200)
+    donor_age = models.IntegerField(default=0)
+    blood_type = models.CharField(max_length=5)
+    blood_status = models.TextField(max_length=60)
+    quantity_donated = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
+
+class BloodDrive(models.Model):
+    drive_owner = models.ForeignKey('Hospital',on_delete=models.CASCADE)
+    drive_title = models.CharField(max_length=150)
+    drive_location = models.TextField(max_length=255)
+    drive_date = models.DateTimeField(auto_now_add=True)
+    capacity_collected = models.IntegerField(default=0)
+    donors = models.ForeignKey('Donor',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.drive_title
--- hospital/views.py
+++ hospital/views.py
@@ -21,7 +21,7 @@ class RegularUserSignUpView(CreateView):
     form_class = RegularUserSignUpForm
     template_name = 'registration/signup_form.html'
     def get_context_data(self, **kwargs):
-        kwargs['user_type'] = 'Regular User'
+        kwargs['user_type'] = 'Donor'
         return super().get_context_data(**kwargs)
     def form_valid(self, form):
         user = form.save()
